73385a5fa958a90c53ba349a347cf7319e1a0e95
NickyLogan <laurentiusdominick@yahoo.com>
2018-07-31 19:46:09 +0700

    Add Item model
    Add tests for Item
    Edit other app configs

--- technostall_manager/apps.py
+++ technostall_manager/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class TechnostallManagerConfig(AppConfig):
     name = 'technostall_manager'
+
+    def ready(self):
+        import technostall_manager.signals
--- technostall_manager/models.py
+++ technostall_manager/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Item(models.Model):
+  item_name = models.CharField(max_length=50)
+  item_price = models.FloatField()
+  created_date = models.DateTimeField(default=timezone.now)
+  description = models.TextField(null=True, blank=True)
+  
+  def __str__(self):
+    return "<Item> %s : Rp %s" % (item_name, item_price)
