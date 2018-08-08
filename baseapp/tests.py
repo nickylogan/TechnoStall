f8515b85861bbe4274867b9f314cb9fa85ca273d
NickyLogan <laurentiusdominick@yahoo.com>
2018-08-08 13:25:17 +0700

    Add login tests

--- baseapp/tests.py
+++ baseapp/tests.py
@@ -1,3 +1,48 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout, get_user
+from django.urls import reverse
+from django.http import HttpRequest
 
-# Create your tests here.
+from .models import TSUser
+
+
+class ViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_index_page(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+        pass
+
+    def test_login_page(self):
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+        pass
+
+
+class LoginTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            'userlogin', 'test@email.com', 'password')
+        self.tsuser = TSUser.objects.create(user=self.user, major=TSUser.INFORMATICS, role=TSUser.STALLKEEPER)
+
+    def test_login_logout(self):
+        response = self.client.post(reverse('login'), {'username': 'userlogin', 'password': 'password'}, follow=True)
+        user = get_user(self.client)
+        self.assertTrue(user.is_authenticated)
+        self.assertRedirects(response, reverse('dashboard'))
+        
+        response = self.client.get(reverse('logout'))
+        user = get_user(self.client)
+        self.assertFalse(user.is_authenticated)
+        self.assertRedirects(response, '/')
+
+
+    def test_login_invalid(self):
+        response = self.client.post(reverse('login'), {'username': 'falseuser', 'password': 'falsepassword'}, follow=True)
+        user = get_user(self.client)
+        self.assertFalse(user.is_authenticated)
+        self.assertEquals(response.request['PATH_INFO'], '/login/')
