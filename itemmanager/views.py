56323de7d79568620b5dc54f8f7075be64062c62
NickyLogan <laurentiusdominick@yahoo.com>
2018-08-15 19:25:31 +0700

    Move decorators from technostall/ to baseapp/

--- itemmanager/views.py
+++ itemmanager/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from technostall.decorators import admin_required
+from baseapp.decorators import admin_required
 from itemmanager.models import Item
 
 @login_required
