0abca42dd02dd09cac565ffec0f78b6099d8e336
NickyLogan <laurentiusdominick@yahoo.com>
2018-08-20 23:59:16 +0700

    Add pricelist template

--- itemmanager/urls.py
+++ itemmanager/urls.py
@@ -3,8 +3,8 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('pricelist/', views.pricelist, name='item_list'),
+    path('pricelist/', views.pricelist, name='pricelist'),
     path('items/new/', views.item_new, name='item_new'),
-    path('items/<int:pk>', views.item_detail, name='item_detail'),
+    path('items/<int:pk>/', views.item_detail, name='item_detail'),
     path('items/<int:pk>/edit', views.item_edit, name='item_edit'),
 ]
--- itemmanager/views.py
+++ itemmanager/views.py
@@ -3,13 +3,32 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from baseapp.decorators import admin_required
 from itemmanager.models import Item
+import math
 
 @login_required
 def pricelist(request):
-    pass
+    filter_pattern = request.GET.get('f', '') or ''
+    pagination = request.GET.get('p', '') or 1
+    try:
+        pagination = int(pagination) - 1
+    except ValueError:
+        pagination = 0
+    pagination = pagination if pagination > 0 else 0
+    item_per_page = 10
+    items = Item.objects.order_by('item_name').filter(item_name__contains=filter_pattern)
+    max_pagination = math.ceil(items.count() / item_per_page)
+    min_item_index = pagination*item_per_page
+    return render(request, 'pricelist.html', {
+        'items': items[min_item_index:min_item_index+item_per_page], 
+        'paginations': range(1,max_pagination+1),
+        'pagination': pagination + 1,
+        'min_item_index': min_item_index,
+        'filter_pattern': filter_pattern,
+        'active_tab': 'item'
+        })
 
 @login_required
-def item_detail(request):
+def item_detail(request, pk):
     pass
 
 @admin_required
