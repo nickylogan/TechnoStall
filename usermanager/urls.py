a2ce446946e6ed143e7b0707e5a79fc576e2f8de
NickyLogan <laurentiusdominick@yahoo.com>
2018-09-24 00:31:11 +0700

    Add user list

--- usermanager/urls.py
+++ usermanager/urls.py
@@ -3,5 +3,9 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('users/', views.user_list, name='user_list'),
+    path('users/', views.UserListView.as_view(), name='user_list'),
+    path('user/new/', views.user_new, name='user_new'),
+    path('user/<int:pk>/', views.user_detail, name='user_detail'),
+    path('user/<int:pk>/edit', views.user_edit, name='user_edit'),
+    path('user/<int:pk>/delete', views.user_delete, name='user_delete'),
 ]
--- usermanager/views.py
+++ usermanager/views.py
@@ -1,5 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils.decorators import method_decorator
+from django.views.generic import TemplateView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
-def user_list(request):
-    return render(request, 'core/construction.html')
+from baseapp.decorators import admin_required
+from baseapp.models import *
+
+from collections import defaultdict
+import math
+
+
+class UserListView(TemplateView):
+    model = TSUser
+    template_name = 'user_list.html'
+
+    def get_context_data(self, *args, **kwargs):
+        pagination = kwargs.get('page')
+        request = kwargs.get('request')
+        try:
+            pagination = max(0, int(pagination) - 1)
+        except ValueError:
+            pagination = 0
+        pagination = pagination if pagination > 0 else 0
+        users_per_page = 10
+        users = TSUser.objects.order_by('id')
+        max_pagination = math.ceil(users.count() / users_per_page)
+        min_user_index = pagination*users_per_page
+        context = {
+            'ts_users': users[min_user_index:min_user_index+users_per_page],
+            'paginations': range(1, max_pagination+1),
+            'pagination': pagination + 1,
+            'min_user_index': min_user_index,
+            'active_tab': 'staff'
+        }
+        return context
+
+    @method_decorator(admin_required)
+    def get(self, request, *args, **kwargs):
+        pagination = request.GET.get('p', '') or 1
+        context = self.get_context_data(page=pagination, request=request)
+        return render(request, self.template_name, context)
+
+def user_detail(request):
+    pass
+
+def user_new(request):
+    pass
+
+def user_edit(request):
+    pass
+
+def user_delete(request):
+    pass
